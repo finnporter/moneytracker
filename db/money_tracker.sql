e4f82d5a78e353f42f56c0e8f5f46ff78470034c
finnporter <iamfinnporter@gmail.com>
2018-05-04 15:26:58 +0100

    creates database and tables

---
+++ db/money_tracker.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS merchants;
+DROP TABLE IF EXISTS categories;
+
+CREATE TABLE categories (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE merchants (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE transactions (
+  id SERIAL8 PRIMARY KEY,
+  date DATE,
+  amount MONEY,
+  merchant_id INT8 REFERENCES merchants(id),
+  category_id INT8 REFERENCES categories(id)
+);
