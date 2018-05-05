1760ab7cc42cea4c8b047b78bd377e4c9055f64b
finnporter <iamfinnporter@gmail.com>
2018-05-05 18:34:36 +0100

    adds save method to Merchant

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,15 @@
 require 'pry-byebug'
 
 require_relative '../models/category'
+require_relative '../models/merchant'
 
 Category.delete_all
 
 category1 = Category.new('name' => 'food')
-
 category1.save
+
+merchant1 = Merchant.new('name' => 'Sainsbury')
+merchant1.save
 
 binding.pry
 nil
--- models/merchant.rb
+++ models/merchant.rb
@@ -1,11 +1,19 @@
-require_relative '..db/sql_runner.rb'
+require_relative '../db/sql_runner.rb'
 
 class Merchant
   
-  attr_reader :id, :name
+  attr_reader :id
+  attr_accessor :name
+
 
   def initialize(options)
-    @id = options['id'].to_i options['id']
+    @id = options['id'].to_i if options['id']
     @name = options['name']
+  end
+
+  def save
+    sql = "INSERT INTO merchants (name) VALUES ($1) returning id"
+    values = [@name]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i
   end
 end
