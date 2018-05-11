fa11303cfc86c522c7005571cd52355a7490f7b0
finnporter <iamfinnporter@gmail.com>
2018-05-11 12:14:16 +0100

    displays index of categories

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
 require 'sinatra'
 require 'sinatra/contrib/all'
 require 'pry-byebug'
-require_relative './controllers/category_controller'
-require_relative './controllers/merchant_controller'
-require_relative './controllers/transaction_controller'
-require_relative './models/transaction'
+require_relative 'controllers/category_controller'
+require_relative 'controllers/merchant_controller'
+require_relative 'controllers/transaction_controller'
+require_relative 'models/transaction'
+
 
 get '/' do
   @transactions = Transaction.find_all
---
+++ controllers/category_controller.rb
@@ -0,0 +1,8 @@
+require 'sinatra'
+require 'sinatra/contrib/all'
+require_relative '../models/category'
+
+get "/categories" do
+  @categories = Category.find_all
+  erb :"category/index"
+end
