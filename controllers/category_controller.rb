cd7a2283d637cd3d6068bacd8f2a00815b6f6a13
finnporter <iamfinnporter@gmail.com>
2018-05-11 12:46:09 +0100

    adds edit feature for categories

--- controllers/category_controller.rb
+++ controllers/category_controller.rb
@@ -2,16 +2,30 @@ require 'sinatra'
 require 'sinatra/contrib/all'
 require_relative '../models/category'
 
+
+# INDEX
 get "/categories" do
   @categories = Category.find_all
   erb :"category/index"
 end
 
+# NEW
 get "/categories/new" do
   erb :"category/new"
 end
 
 post "/categories" do
   Category.new(params).save
+  redirect to "/categories"
+end
+
+# EDIT
+get "/categories/:id/edit" do
+  @category = Category.find_all[0]
+  erb :"category/edit"
+end
+
+post "/categories/:id" do
+  Category.new(params).update
   redirect to "/categories"
 end
