9f29b165cfb9f26a163fe0e88a7df1ba7387fbc4
finnporter <iamfinnporter@gmail.com>
2018-05-14 21:09:42 +0100

    creates budget class and save, update, find_all methods

---
+++ models/budget.rb
@@ -0,0 +1,39 @@
+require_relative '../db/sql_runner.rb'
+
+class Budget
+	
+	attr_reader :id
+	attr_accessor :current_budget
+
+	def initialize options
+		@id = options['id'].to_i if options['id']
+		@current_budget = options['current_budget']
+	end
+
+	def save
+    sql = "INSERT INTO budgets (current_budget) VALUES ($1) RETURNING id"
+    values = [@current_budget]
+    results = SqlRunner.run(sql, values)
+    @id = results[0]['id'].to_i
+  end
+
+  def update
+    sql = "UPDATE budgets SET current_budget = $1 WHERE id = $2"
+    values = [@current_budget, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  # ***** CLASS METHODS ***** #
+
+  def self.find_all
+    sql = "SELECT * FROM budgets"
+    results = SqlRunner.run(sql)
+    all_categories = map_helper(results)
+  end
+
+  # ***** HELPER METHODS ***** #
+
+  def self.map_helper results
+    results.map { |result| Budget.new(result) }
+  end
+end
